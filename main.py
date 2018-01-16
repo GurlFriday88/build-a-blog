af071ab65990635b9a1cd61a49c6ed302ea1c5c7
GurlFriday88 <entrotlek@gmail.com>
2018-01-16 01:41:33 -0500

    finished project

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, request, render_template, redirect, url_for
+from flask import Flask, request, request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-
 
 app = Flask(__name__)
 app.config['DEBUG']=True
@@ -10,14 +8,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:haibane@lo
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-
+#database model
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     subtitle = db.Column(db.String(50))
     author = db.Column(db.String(30))
-    date = db.Column(db.Integer)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text)
 
     def __init__(self, title, subtitle, author, date, content):
@@ -27,7 +25,7 @@ class Blog(db.Model):
         self.date = date
         self.content = content
 
-
+#routes
 @app.route('/index')
 def index():
     current_posts = Blog.query.order_by(Blog.id.desc()).all()
@@ -38,24 +36,24 @@ def post(new_post_id):
     new_post = Blog.query.filter_by(id=new_post_id).first_or_404()
     return render_template('post.html', new_post=new_post)
 
-
+#processes post form and adds entry to database
 @app.route('/add_post', methods=['GET','POST'])
 def add_post():
     if request.method == 'POST':
-        new_title = request.form['title']
+        new_title = request.form['title']    
+        if not new_title:
+            error = "Please enter a title."
+            return render_template('add_post.html', error=error)
+
+        new_subtitle = request.form['subtitle']
+        new_author = request.form['author']
+        new_content = request.form['content']
         date = datetime.now()
-        if new_title in new_title:
-            new_post = Blog(
-            new_title, new_subtitle=request.form['subtitle'], new_author=request.form['author'],
-            date,
-            new_content=request.form['content']
-             )
-            db.session.add(new_post)
-            db.session.commit()
-            return redirect(url_for('index'))
-        else:
-            flash("Don't forget the title!")
-            return redirect(url_for('index'))
+        new_post = Blog(new_title, new_subtitle, new_author, date, new_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('index'))
+                
     return render_template('add_post.html')
 
 
